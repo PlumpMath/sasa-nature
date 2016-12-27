489acd3cd2a2cc385f04a71e9f731af6f0f3a369
StudioDotH <studiodoth@protonmail.com>
2016-12-27 11:37:58 +0900

    fixed error

--- js/study_detail.js
+++ js/study_detail.js
@@ -18,7 +18,7 @@ $(document).ready(function()
     }).fail(function() {reject();});
   });
   p.then(function(result) {
-    var docFileName = doc[documentID]+".pdf";
+    var docFileName = params["id"]+".pdf";
     PDFObject.embed("docs/"+docFileName, "#pdf-wrapper");
 
     $('#study-title').html(doc["name"]);
